d7c76cba529f5d88a2f859bd06ec11e4b6fe0224
ravinderkumar <ravindersanjay@gmail.com>
2023-06-27 17:59:31 +0530

    first commit

---
+++ jinkinsTest/src/main/java/com/qa/test/jinkinsTest/App.java
@@ -0,0 +1,13 @@
+package com.qa.test.jinkinsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
